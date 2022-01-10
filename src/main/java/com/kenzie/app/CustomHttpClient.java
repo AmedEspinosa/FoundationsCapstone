f0efc58233af59eb2be60c2953afceeb9f6e0669
AmedEspinosa <amed.espinosa@mykenzie.snhu.edu>
2022-01-09 19:23:20 -0500

    used all I learned in the foundation to run efficient clean code

--- src/main/java/com/kenzie/app/CustomHttpClient.java
+++ src/main/java/com/kenzie/app/CustomHttpClient.java
@@ -1,18 +1,81 @@
 package com.kenzie.app;
 
+// This comes from the code in the reading
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomHttpClient {
 
     //TODO: Write sendGET method that takes URL and returns response
-    public static String sendGET(String URLString) {
-        //** Start of GET request algorithm
 
-        return "";
+    //Standard GET request borrowed from the reading
+    public String sendGET(String URLString) {
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create(URLString);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            if (statusCode == 200) {
+                return httpResponse.body();
+            } else {
+
+                return String.format("GET request failed: %d status code received", statusCode);
+            }
+        } catch (InterruptedException | IOException e) {
+            return e.getMessage();
+        }
+    }
+
+// Custom method to format my JSON file to only account for what my program needs
+    // mirrored previous assignments and examples from the readings
+
+    public String formatCluesDTO (String jsonString)throws JsonProcessingException{
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        CluesDTO cluesDTO = objectMapper.readValue(jsonString,CluesDTO.class);
+
+        StringBuilder cluesString = new StringBuilder();
+        cluesString.append("Category: ")
+                .append(cluesDTO.getCategory().getTitle())
+                .append("\n")
+                .append("Question: ")
+                .append(cluesDTO.getQuestion());
+
+        return cluesString.toString();
+    }
+
+
+    // Custom method to check if the user inputted response is equal to the correct answer
+    public String answerCheck (String jsonString, String userAnswer)throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        CluesDTO cluesDTO = objectMapper.readValue(jsonString,CluesDTO.class);
+        String correctAnswer = cluesDTO.getAnswer();
+
+        StringBuilder sbCorrect = new StringBuilder("Congrats! Your Answer was correct!");
+
+        StringBuilder sbIncorrect = new StringBuilder("Sorry! Your answer was incorrect!");
+        sbIncorrect.append("\n")
+                .append("The correct answer was: " + correctAnswer);
+
+        if (userAnswer.equalsIgnoreCase(correctAnswer)) {
+            return sbCorrect.toString();
+        } else return sbIncorrect.toString();
     }
 }
 
--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -1,6 +1,11 @@
 package com.kenzie.app;
 
 // import necessary libraries
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 
 public class Main {
@@ -15,9 +20,153 @@ public class Main {
 
      */
 
+    // Method to ask user how many players are going to play and the game itself //
 
-    public static void main(String[] args)  {
-        //Write main execution code here
+    public static String gameMethod(int userChoice) throws JsonProcessingException {
+        int count = 0;
+        int userScore1 = 0;
+        int userScore2 = 0;
+
+        int userSingleScore = 0;
+
+        // if statement when user selects 2 player mode
+
+        if (userChoice == 2) {
+
+            // using a while loop to loop 10 times before exiting
+            while (count < 10) {
+                count += 1;
+
+                CustomHttpClient myHttpClient = new CustomHttpClient();
+
+                int randomNum1 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+                int randomNum2 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+
+
+            //////////////////////////////// PLAYER 1 //////////////////////////////////////////////
+
+
+                // Building the URL to incorporate the random number generator
+                StringBuilder sb1 = new StringBuilder();
+                sb1.append("https://jservice.kenzie.academy/api/clues/");
+                sb1.append(randomNum1);
+                String url1 = sb1.toString();
+
+
+
+                // Using my methods to read from the JSON data
+                System.out.println("Player 1's turn!");
+                String cluesResponseBody1 = myHttpClient.sendGET(url1);
+                System.out.println(myHttpClient.formatCluesDTO(cluesResponseBody1));
+
+
+                // scanner to read user input
+                Scanner scanner = new Scanner(System.in);
+                String userResponse1 = scanner.nextLine();
+
+
+                // Using my custom method & if statement to check for accurate user response
+                String answerCheckString = myHttpClient.answerCheck(cluesResponseBody1, userResponse1);
+                String compareString = "Congrats! Your Answer was correct!";
+
+
+                if (answerCheckString.equalsIgnoreCase(compareString)) {
+                    userScore1 += 1;
+                    System.out.println(answerCheckString);
+                    System.out.println("Player 1's score: " + userScore1);
+                } else System.out.println(answerCheckString);
+
+
+
+                ///////////////////////////////// PLAYER 2 ///////////////////////////////////////
+
+
+                // Building the URL to incorporate the random number generator
+                StringBuilder sb2 = new StringBuilder();
+                sb2.append("https://jservice.kenzie.academy/api/clues/");
+                sb2.append(randomNum2);
+                String url2 = sb2.toString();
+
+
+                // Using my methods to read from the JSON data
+                System.out.println("Player 2's Turn");
+                String cluesResponseBody2 = myHttpClient.sendGET(url2);
+                System.out.println(myHttpClient.formatCluesDTO(cluesResponseBody2));
+
+                // scanner to read user input
+                String userResponse2 = scanner.nextLine();
+
+                // Using my custom method & if statement to check for accurate user response
+                String answerCheckString2 = myHttpClient.answerCheck(cluesResponseBody2, userResponse2);
+
+                if (answerCheckString2.equalsIgnoreCase(compareString)) {
+                    userScore2 += 1;
+                    System.out.println(answerCheckString2);
+                    System.out.println("Player 2's score: " + userScore2);
+                } else System.out.println(answerCheckString2);
+            }
+            // Exit Message
+            return  "Thanks for playing!" + "\n" + "Player 1's final score was: " + userScore1 +
+                    "\n" + "Player 2's final score was: " + userScore2;
+
+
+
+
+            //////////////////////////////////////// SINGLE USER /////////////////////////////////////////
+
+        } else
+            while (count < 10) {
+            count += 1;
+
+            CustomHttpClient myHttpClient = new CustomHttpClient();
+
+            int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+
+            // Building the URL to incorporate the random number generator
+            StringBuilder sb = new StringBuilder();
+            sb.append("https://jservice.kenzie.academy/api/clues/");
+            sb.append(randomNum);
+            String url = sb.toString();
+
+
+            // Using my methods to read from the JSON data
+            String cluesResponseBody = myHttpClient.sendGET(url);
+            System.out.println(myHttpClient.formatCluesDTO(cluesResponseBody));
+
+
+            // scanner to read user input
+            Scanner scanner = new Scanner(System.in);
+            String userResponse = scanner.nextLine();
+
+
+            // Using my custom method & if statement to check for accurate user response
+            String answerCheckString = myHttpClient.answerCheck(cluesResponseBody, userResponse);
+            String compareString = "Congrats! Your Answer was correct!";
+
+
+            if (answerCheckString.equalsIgnoreCase(compareString)) {
+                userSingleScore += 1;
+                System.out.println(answerCheckString);
+                System.out.println("User Score: " + userSingleScore);
+            } else System.out.println(answerCheckString);
+        }
+        // Exit Message
+        return  "Thanks for playing!" + "\n" + "Your final score was: " + userSingleScore;
+
     }
-}
+
+    public static void main(String[] args) throws JsonProcessingException {
+        //Write main execution code here
+
+            System.out.println("Hello welcome to the game!" + "\n" + "Input the number of players: 1 or 2");
+
+            Scanner scanner = new Scanner(System.in);
+
+            int userChoice = scanner.nextInt();
+
+            System.out.println(gameMethod(userChoice));
+
+        }
+    }
+
 
